1de85ee47b24c6d00867c9f75bdbd638f47c7bf6
Wayne Lui <waynelui@outlook.com>
2022-10-16 23:30:38 +0800

    Change assembly version to constant string

--- src/Axiom/MainWindow.xaml.cs
+++ src/Axiom/MainWindow.xaml.cs
@@ -217,10 +217,10 @@ namespace Axiom
             // -------------------------
             // Set Current Version to Assembly Version
             // -------------------------
-            System.Reflection.Assembly assembly = System.Reflection.Assembly.GetExecutingAssembly();
-            FileVersionInfo fvi = FileVersionInfo.GetVersionInfo(assembly.Location);
-            string assemblyVersion = fvi.FileVersion;
-            currentVersion = new Version(assemblyVersion);
+            //System.Reflection.Assembly assembly = System.Reflection.Assembly.GetExecutingAssembly();
+            //FileVersionInfo fvi = FileVersionInfo.GetVersionInfo(assembly.Location);
+            //string assemblyVersion = fvi.FileVersion;
+            currentVersion = new Version("1.9.6.1");
 
             // -------------------------
             // Start the Log Console (Hidden)
